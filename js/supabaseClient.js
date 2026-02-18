be9af1ec9ebf4ebf2b22700a8aa12727fe5e8277
Aenesche <142432255+Aenesche@users.noreply.github.com>
2026-02-18 09:55:24 +0100

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,1 +1,8 @@
 
+
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+
+const SUPABASE_URL = "https://suhqzygjzedopqcwttpz.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_B3Kv9E-EqAsiG0PwWlJNMQ_yNC76Lqx";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
