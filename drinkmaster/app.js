25695948037765680309af39c8e6c85ee2d78561
Aenesche <142432255+Aenesche@users.noreply.github.com>
2026-03-16 10:40:42 +0100

    Update app.js

--- drinkmaster/app.js
+++ drinkmaster/app.js
@@ -1,6 +1,6 @@
 // Ersetze diese durch deine ECHTEN Supabase Keys aus den Projekteinstellungen
-const SUPABASE_URL = 'https://deine-projekt-id.supabase.co';
-const SUPABASE_ANON_KEY = 'dein-anon-key';
+const SUPABASE_URL = 'https://usihbregbanpfspblrnw.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzaWhicmVnYmFucGZzcGJscm53Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMDkyNzEsImV4cCI6MjA4NzY4NTI3MX0.U_f2brykxMbegtddye-hpy0lcJgtEzl1AB9lQGpd5UY';
 
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
