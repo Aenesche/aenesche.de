c43206c9a8330f3a5b721dc23e3df6393c6c5e9a
Aenesche <142432255+Aenesche@users.noreply.github.com>
2026-03-16 11:24:10 +0100

    Update party.js

--- drinkmaster/party.js
+++ drinkmaster/party.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = 'https://usihbregbanpfspblrnw.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzaWhicmVnYmFucGZzcGJscm53Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMDkyNzEsImV4cCI6MjA4NzY4NTI3MX0.U_f2brykxMbegtddye-hpy0lcJgtEzl1AB9lQGpd5UY';
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // URL Parameter auslesen
 const urlParams = new URLSearchParams(window.location.search);
