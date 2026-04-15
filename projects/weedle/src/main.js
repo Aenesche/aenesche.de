6ababfaac1d404aa953fa9050a9f758a86bc72ed
Aenesche <142432255+Aenesche@users.noreply.github.com>
2026-04-15 16:05:56 +0200

    Create main.js

---
+++ projects/weedle/src/main.js
@@ -0,0 +1,24 @@
+// Einstiegspunkt. Phaser-Config + Scenes registrieren.
+
+import { GAME } from './config/constants.js';
+import BootScene from './scenes/BootScene.js';
+import GameScene from './scenes/GameScene.js';
+
+const config = {
+    type: Phaser.AUTO,
+    parent: 'game',
+    width: GAME.WIDTH,
+    height: GAME.HEIGHT,
+    backgroundColor: GAME.BG,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    physics: {
+        default: 'arcade',
+        arcade: { gravity: { y: 0 }, debug: false },
+    },
+    scene: [BootScene, GameScene],
+};
+
+new Phaser.Game(config);
