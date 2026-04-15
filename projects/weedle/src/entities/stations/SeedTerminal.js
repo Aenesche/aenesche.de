2874155d048a20095de8c1c7a8ed1279d747dddc
Aenesche <142432255+Aenesche@users.noreply.github.com>
2026-04-15 23:10:34 +0200

    Update SeedTerminal.js

--- projects/weedle/src/entities/stations/SeedTerminal.js
+++ projects/weedle/src/entities/stations/SeedTerminal.js
@@ -1,39 +1,26 @@
-// Samen-Terminal: oranger Würfel mit gelbem Holo-Samen.
-// Aktuell rein dekorativ — Interaktion (Hold-to-buy) kommt später.
-
+import Station from './Station.js';
 import { COLORS, ISO } from '../../config/constants.js';
-import { gridToIso, drawIsoCube } from '../../utils/iso.js';
+import { drawIsoCube } from '../../utils/iso.js';
 
 const HEIGHT = 30;
 
-export default class SeedTerminal {
-    constructor(scene, gridX, gridY) {
-        this.scene = scene;
-        this.gridX = gridX;
-        this.gridY = gridY;
-        this.draw();
-    }
-
-    getTiles() {
-        return [{ x: this.gridX, y: this.gridY }];
-    }
-
-    draw() {
-        const g = this.scene.add.graphics();
-        const pos = gridToIso(this.gridX, this.gridY, this.scene.originX, this.scene.originY);
-
-        // Würfel
-        drawIsoCube(g, pos.x, pos.y, ISO.TILE_SIZE, HEIGHT, COLORS.SEED_SHOP, 0.2, 0.1);
+export default class SeedTerminal extends Station {
+    drawSelf(g) {
+        drawIsoCube(g, this.isoX, this.isoY, ISO.TILE_SIZE, HEIGHT, COLORS.SEED_SHOP, 0.2, 0.1);
 
         // Samen-Hologramm über dem Deckel
-        const cx = pos.x;
-        const cy = pos.y + ISO.TILE_SIZE / 2 - HEIGHT - 10;
+        const cx = this.isoX;
+        const cy = this.isoY + ISO.TILE_SIZE / 2 - HEIGHT - 10;
         g.lineStyle(2, 0xffff00, 1);
         g.strokeEllipse(cx, cy, 16, 8);
         g.fillStyle(0xffff00, 1);
         g.fillCircle(cx, cy, 3);
 
-        // Depth = Footprint-Center auf dem Boden, damit Stationen im Iso korrekt sortieren
-        g.setDepth(pos.y + ISO.TILE_SIZE / 2);
+        this.bounds = {
+            left:   this.isoX - ISO.TILE_SIZE,
+            right:  this.isoX + ISO.TILE_SIZE,
+            top:    this.isoY - HEIGHT - 20,
+            bottom: this.isoY + ISO.TILE_SIZE,
+        };
     }
 }
