f8c87da6b03b43225b1c521d38c305f42c42e925
Aenesche <142432255+Aenesche@users.noreply.github.com>
2026-04-16 01:56:30 +0200

    Update SeedTerminal.js

--- projects/weedle/src/entities/stations/SeedTerminal.js
+++ projects/weedle/src/entities/stations/SeedTerminal.js
@@ -1,6 +1,7 @@
 import Station from './Station.js';
-import { COLORS, ISO } from '../../config/constants.js';
+import { COLORS, ISO, ECONOMY, ITEMS } from '../../config/constants.js';
 import { drawIsoCube } from '../../utils/iso.js';
+import CarriedItem from '../CarriedItem.js';
 
 const HEIGHT = 30;
 
@@ -24,11 +25,20 @@ export default class SeedTerminal extends Station {
     }
 
     getInteraction() {
+        const player = this.scene.player;
+        const state = this.scene.state;
+
+        // Bedingungen: Player muss leere Hände haben UND genug Geld
+        if (player.hasItem()) return null;
+        if (!state.canAfford(ECONOMY.SEED_COST)) return null;
+
         return {
             type: 'hold',
             duration: 2000,
             onComplete: () => {
-                console.log('🌱 Samen gekauft (Platzhalter)');
+                if (!state.spend(ECONOMY.SEED_COST)) return;
+                const seed = new CarriedItem(this.scene, ITEMS.SEED);
+                player.pickUp(seed);
             },
         };
     }
