cf333eb94838cb85d39a5849bdf853ac76bb5f84
Aenesche <142432255+Aenesche@users.noreply.github.com>
2026-04-17 17:13:04 +0200

    Create HiringStation.js

---
+++ projects/weedle/src/entities/stations/HiringStation.js
@@ -0,0 +1,30 @@
+import Station from './Station.js';
+import { COLORS, ISO, EMPLOYEE } from '../../config/constants.js';
+import { drawIsoCube } from '../../utils/iso.js';
+
+const HEIGHT = 25;
+
+export default class HiringStation extends Station {
+    drawSelf(g) {
+        drawIsoCube(g, this.isoX, this.isoY, ISO.TILE_SIZE, HEIGHT, COLORS.EMPLOYEE, 0.2, 0.1);
+
+        // Person-Icon auf dem Deckel
+        const cx = this.isoX;
+        const cy = this.isoY + ISO.TILE_SIZE / 2 - HEIGHT - 5;
+        g.fillStyle(COLORS.EMPLOYEE, 1);
+        g.fillCircle(cx, cy - 4, 4);
+        g.fillRect(cx - 4, cy, 8, 6);
+
+        this.bounds = {
+            left:   this.isoX - ISO.TILE_SIZE,
+            right:  this.isoX + ISO.TILE_SIZE,
+            top:    this.isoY - HEIGHT - 15,
+            bottom: this.isoY + ISO.TILE_SIZE,
+        };
+    }
+
+    // Keine E-Interaktion, nur Q-Popup
+    getInteraction() {
+        return null;
+    }
+}
