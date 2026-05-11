fb6d784b1aea7f26d35946a8b671c48ce0334da9
Aenesche <142432255+Aenesche@users.noreply.github.com>
2026-05-11 18:00:12 +0200

    Add terminal types to builtCount in BuildManager

--- projects/weedle/src/world/BuildManager.js
+++ projects/weedle/src/world/BuildManager.js
@@ -12,25 +12,32 @@ export default class BuildManager {
         this.slots = [];
 
         // Pro Typ: wie viele davon existieren (Start-Stationen zählen)
-        this.builtCount = { bed: 0, register: 0, storage: 0, hiring: 0, trash: 0 };
-    }
+this.builtCount = {
+            bed: 0, register: 0, storage: 0, hiring: 0, trash: 0,
+            terminal_mint: 0, terminal_haze: 0, terminal_kush: 0,
+            terminal_crystal: 0, terminal_og: 0,
+        };    }
 
     // Wird von der Scene aufgerufen, nachdem Start-Stationen platziert sind
     init(startStations) {
         // Zähle existierende Stationen
         for (const s of startStations) {
             const name = s.constructor.name.toLowerCase();
-            if (name.includes('bed')) this.builtCount.bed++;
+            if (name.includes('bed') && !name.includes('tier')) this.builtCount.bed++;
             else if (name.includes('register')) this.builtCount.register++;
             else if (name.includes('storage')) this.builtCount.storage++;
             else if (name.includes('hiring')) this.builtCount.hiring++;
+            else if (name.includes('seedterminal') || name.includes('terminal')) {
+                // Sorte aus dem Station-Objekt lesen
+                if (s.variety) this.builtCount[`terminal_${s.variety.id}`]++;
+            }
         }
 
         // BuildSlots aus der Config erstellen.
         // Überspringe Positionen die schon belegt sind (Start-Stationen).
         const occupied = new Set(startStations.map(s => `${s.gridX},${s.gridY}`));
-        const indexPerType = { bed: 0, register: 0, storage: 0, hiring: 0, trash: 0 };
-
+        const indexPerType = { ...this.builtCount };
+        
         for (const def of BUILD.SLOTS) {
             const key = `${def.gridX},${def.gridY}`;
             if (occupied.has(key)) {
