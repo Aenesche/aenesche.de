ab846a260d8edeee5d41dce1fdc6e5e864513501
Aenesche <142432255+Aenesche@users.noreply.github.com>
2026-05-12 13:54:07 +0000

    Simplify drawing logic for carried items

--- projects/weedle/src/entities/CarriedItem.js
+++ projects/weedle/src/entities/CarriedItem.js
@@ -17,17 +17,11 @@ export default class CarriedItem {
         const g = this.graphics;
         g.clear();
 
-        if (this.itemDef.id === ITEMS.SEED.id) {
-            // Stilisierter Samen: gelbes Oval mit Outline
-            g.fillStyle(this.itemDef.color, 1);
-            g.fillEllipse(0, 0, 10, 6);
-            g.lineStyle(1.5, 0xffffff, 0.8);
-            g.strokeEllipse(0, 0, 10, 6);
-        } else {
-            // Generisches Item-Visual als Fallback
-            g.fillStyle(this.itemDef.color, 1);
-            g.fillCircle(0, 0, 5);
-        }
+        // Alle Items gleich zeichnen: farbiges Oval mit Outline
+        g.fillStyle(this.itemDef.color, 1);
+        g.fillEllipse(0, 0, 10, 6);
+        g.lineStyle(1.5, 0xffffff, 0.8);
+        g.strokeEllipse(0, 0, 10, 6);
     }
 
     destroy() {
