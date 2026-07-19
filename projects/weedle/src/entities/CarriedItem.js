aae4d66533634328d09b4946588d94afd82d04a7
Aenesche <142432255+Aenesche@users.noreply.github.com>
2026-07-19 13:55:05 +0000

    Update CarriedItem.js

--- projects/weedle/src/entities/CarriedItem.js
+++ projects/weedle/src/entities/CarriedItem.js
@@ -17,11 +17,29 @@ export default class CarriedItem {
         const g = this.graphics;
         g.clear();
 
-        // Alle Items gleich zeichnen: farbiges Oval mit Outline
-        g.fillStyle(this.itemDef.color, 1);
-        g.fillEllipse(0, 0, 10, 6);
-        g.lineStyle(1.5, 0xffffff, 0.8);
-        g.strokeEllipse(0, 0, 10, 6);
+        if (this.itemDef.id.startsWith('seed_')) {
+            // Samen: gelber Tropfen (wie der Ur-Samen), Outline in Sorten-Farbe
+            g.fillStyle(0xffff00, 1);
+            g.lineStyle(2, this.itemDef.color, 1);
+            g.beginPath();
+            g.moveTo(0, -6);
+            g.lineTo(-5, 3);
+            g.lineTo(5, 3);
+            g.closePath();
+            g.fillPath();
+            g.strokePath();
+        } else if (this.itemDef.id === 'rotten') {
+            // Verfault: rotes X
+            g.lineStyle(2.5, this.itemDef.color, 1);
+            g.beginPath(); g.moveTo(-5, -5); g.lineTo(5, 5); g.strokePath();
+            g.beginPath(); g.moveTo(5, -5); g.lineTo(-5, 5); g.strokePath();
+        } else {
+            // Pflanze: Oval in Sorten-Farbe mit weißer Outline
+            g.fillStyle(this.itemDef.color, 1);
+            g.fillEllipse(0, 0, 10, 6);
+            g.lineStyle(1.5, 0xffffff, 0.8);
+            g.strokeEllipse(0, 0, 10, 6);
+        }
     }
 
     destroy() {
